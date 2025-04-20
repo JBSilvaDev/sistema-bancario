f2878ff2994888e79f146ebee820b8b15245c5ab
Juliano Silva <jbsilva.dev@outlook.com>
2025-04-19 22:32:52 -0300

    criando contas

--- criar_contas.py
+++ criar_contas.py
@@ -9,15 +9,24 @@ def criar_conta (nome, login, senha, saldo_inicial=0):
     id INTEGER PRIMARY KEY AUTOINCREMENT, 
     nome TEXT NOT NULL, 
     login TEXT NOT NULL, 
-    senha TEXT NOT NULL
+    senha TEXT NOT NULL,
+    saldo REAL NOT NULL
   )''')
 
+  print('Criando conta...')
   querys.execute('''
                   INSERT INTO contas (nome, login, senha, saldo)
                   VALUES (?, ?, ?, ?)
                   ''', (nome, login, senha, saldo_inicial))
   
+
+  querys.execute('SELECT * FROM contas WHERE login = ? AND senha = ?', (login, senha))
+  conta = querys.fetchone()
+  print('Conta criada com sucesso!, seu código de conta é: ', conta[0])
+  
   con.commit()
   querys.close()
   con.close()
   
+
+criar_conta('Guilhermes', 'guilherme', '12356', 0)
