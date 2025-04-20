f6ec6923214501c59387c3d4bcfa20f13ca43833
Juliano Silva <jbsilva.dev@outlook.com>
2025-04-19 23:24:12 -0300

    testando projeto

--- conexao_bd.py
+++ conexao_bd.py
@@ -17,14 +17,12 @@ def login(cont_numero:int, senha:str = None):
       print(f'Acessando conta {cont_numero}...')
       querys.execute('SELECT * FROM contas WHERE id = ? AND senha = ?', (cont_numero, senha, ))
       conta = querys.fetchone()
+      if conta is None:
+        print('Conta inexistente ou senha incorreta.')
+        return 
       print(f'Bem-vindo(a) {conta[1]}!')
 
-    if conta is None:
-      print('Conta inexistente.')
-      return 
-    
   except:
-    print('Erro ao acessar o banco de dados.')
     return False
 
   con.commit()
